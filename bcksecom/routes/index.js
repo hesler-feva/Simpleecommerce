3918332f01ae97576300309be7f4b49ccb37899b
Hesler Ferrera <heslerferrera@gmail.com>
2020-09-27 09:50:17 -0600

    20200924

--- bcksecom/routes/index.js
+++ bcksecom/routes/index.js
@@ -1,3 +1,4 @@
+//incroporacion de dependencias
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +6,32 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get(
+  '/info', 
+  function(req, res){
+    var infoObj = {}; //$infoObj = array();
+    infoObj.cuenta = "0817199800256";
+    infoObj.nombre = "Hesler Ferrera";
+    infoObj.email = "heslerferrera@gmail.com";
+    res.json(infoObj);
+  }
+);
+
+router.get(
+  '/hook',
+  function(req, res){
+    res.render("hook", {});
+  }
+);
+
+router.post(
+  '/hook',
+  function(req, res){
+    var body = req.body;
+    console.log(body);
+    res.render("hook", body);
+  }
+);
 
 module.exports = router;
